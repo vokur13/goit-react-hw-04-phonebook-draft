0a7a6333da8b353ec9ee2a4e12a1ed697f17d436
vokur13 <vokur13@gmail.com>
2022-10-15 18:19:18 +0300

    localStorage added

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,24 +10,20 @@ import { save, load } from '../utils/storage';
 const STORAGE_KEY = 'contacts';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(window.localStorage.getItem(STORAGE_KEY)) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
   const [filter, setFilter] = useState('');
 
-  //   useEffect(() => {
-  //     if (load(STORAGE_KEY)) {
-  //       setContacts(load(STORAGE_KEY));
-  //     }
-  //     setContacts(save(STORAGE_KEY, contacts));
-  //   }, [contacts]);
-
-  //   useEffect(() => {
-  //     setContacts(save(STORAGE_KEY, contacts));
-  //   }, [contacts]);
+  useEffect(() => {
+    window.localStorage.setItem(STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   function formSubmitHandler({ name, number }) {
     const checkName = contacts.some(item =>
